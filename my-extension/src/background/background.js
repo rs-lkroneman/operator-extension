d83eedf3011c755dbfda2b8164a3e13f8eed9982
Lennart Kroneman <lennart.kroneman@daveramsey.com>
2021-05-06 10:04:11 -0500

    fix missing commands issue and improve search

--- my-extension/src/background/background.js
+++ my-extension/src/background/background.js
@@ -3,22 +3,31 @@ import commandHandlers, { commandNames } from './commands';
 
 const memory = {};
 const runCommand = (command) => {
+    if(command === "REFRESH_COMMANDS") {
+        refreshCommands();
+        return;
+    }
+
     if(Object.hasOwnProperty.call(commandHandlers, command)) {
         commandHandlers[command]();
     }
 }
 
-chrome.runtime.onInstalled.addListener(() => {
+const refreshCommands = () => {
     chrome.commands.getAll(result => {
-        memory.commands = [...commandNames];
+        memory.commands = Array.from(new Set([...commandNames, "REFRESH_COMMANDS"]));
     });
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+    refreshCommands();
 
     chrome.commands.onCommand.addListener(runCommand);
 });
 
 
 chrome.extension.onConnect.addListener((port) => {
-    console.log("Connected .....");
+    refreshCommands();
     port.postMessage(memory.commands);
     port.onMessage.addListener(runCommand);
 });
