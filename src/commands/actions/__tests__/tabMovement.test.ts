ee0bd807d1f9f1ba4c15a3ba3e0d830e67b4ea90
Lennart Kroneman <a.l.kroneman@gmail.com>
2022-01-08 17:12:06 -0600

    basic tests around tab movement

--- src/commands/actions/__tests__/tabMovement.test.ts
+++ src/commands/actions/__tests__/tabMovement.test.ts
@@ -1,58 +1,141 @@
-import tabMovement from 'src/commands/actions/tabMovement';
+import tabMovement from "src/commands/actions/tabMovement";
 import chromeTabs from "src/api/tabs";
-import {buildTab, indexToTab, createMockQueryImplementationWith} from "test_utils/tabs";
+import {
+  buildTab,
+  indexToTab,
+  createMockQueryImplementationWith,
+} from "test_utils/tabs";
 
 jest.mock("src/api/tabs");
 
 const DEFAULT_TABS = [
   buildTab(),
-  buildTab({highlighted: true, id: 9}),
-  buildTab()
+  buildTab({ highlighted: true, id: 9 }),
+  buildTab(),
 ].map(indexToTab);
 
-describe('TabMovement', () => {
+describe("TabMovement", () => {
   beforeEach(() => {
     jest.resetAllMocks();
-  })
+  });
 
-  describe('When all tabs are unpinned', () => {
-    it('moves the tab left if it is not the first', async () => {
-      (chromeTabs.query as jest.Mock).mockImplementation(createMockQueryImplementationWith(DEFAULT_TABS));
+  describe("Moving a single tab to the left", () => {
+    describe("When all tabs are unpinned", () => {
+      it("moves the tab left if it is not the first", async () => {
+        (chromeTabs.query as jest.Mock).mockImplementation(
+          createMockQueryImplementationWith(DEFAULT_TABS)
+        );
 
-      await tabMovement('move_tab_left');
-      expect(chromeTabs.move).toHaveBeenCalledWith(9, {index: 0})
+        await tabMovement("move_tab_left");
+        expect(chromeTabs.move).toHaveBeenCalledWith(9, { index: 0 });
+      });
+
+      it("moves the tab to the end if it is the first", async () => {
+        const TAB_STATE = [
+          buildTab({ highlighted: true, id: 9 }),
+          buildTab(),
+          buildTab(),
+        ].map(indexToTab);
+
+        (chromeTabs.query as jest.Mock).mockImplementation(
+          createMockQueryImplementationWith(TAB_STATE)
+        );
+
+        await tabMovement("move_tab_left");
+        expect(chromeTabs.move).toHaveBeenCalledWith(9, { index: 2 });
+      });
     });
+  });
 
-    it('moves the tab to the end if it is the first', async () => {
-      const WITH_FIRST_TAB_ACTIVE = [
-        buildTab({highlighted: true, id: 9}),
+  describe("Moving a single tab to the right", () => {
+    describe("When all tabs are unpinned", () => {
+      it("moves the tab right if it is not the last", async () => {
+        (chromeTabs.query as jest.Mock).mockImplementation(
+          createMockQueryImplementationWith(DEFAULT_TABS)
+        );
+
+        await tabMovement("move_tab_right");
+        expect(chromeTabs.move).toHaveBeenCalledWith(9, { index: 2 });
+      });
+
+      it("moves the tab to the start if it is the last", async () => {
+        const TAB_STATE = [
+          buildTab(),
+          buildTab(),
+          buildTab({ highlighted: true, id: 9 }),
+        ].map(indexToTab);
+
+        (chromeTabs.query as jest.Mock).mockImplementation(
+          createMockQueryImplementationWith(TAB_STATE)
+        );
+
+        await tabMovement("move_tab_right");
+        expect(chromeTabs.move).toHaveBeenCalledWith(9, { index: 0 });
+      });
+    });
+  });
+
+  describe("Move a single tab To Start", () => {
+    it("moves it to the start even if it is already there", async () => {
+      const TAB_STATE = [
+        buildTab({ highlighted: true, id: 9 }),
         buildTab(),
-        buildTab()
+        buildTab(),
       ].map(indexToTab);
 
-      (chromeTabs.query as jest.Mock).mockImplementation(createMockQueryImplementationWith(WITH_FIRST_TAB_ACTIVE));
+      (chromeTabs.query as jest.Mock).mockImplementation(
+        createMockQueryImplementationWith(TAB_STATE)
+      );
 
-      await tabMovement('move_tab_left');
-      expect(chromeTabs.move).toHaveBeenCalledWith(9, {index: 2})
+      await tabMovement("move_tab_to_front");
+      expect(chromeTabs.move).toHaveBeenCalledWith(9, { index: 0 });
+    });
+
+    it("moves it to the start if in the middle", async () => {
+      const TAB_STATE = [
+        buildTab(),
+        buildTab({ highlighted: true, id: 9 }),
+        buildTab(),
+      ].map(indexToTab);
+
+      (chromeTabs.query as jest.Mock).mockImplementation(
+        createMockQueryImplementationWith(TAB_STATE)
+      );
+
+      await tabMovement("move_tab_to_front");
+      expect(chromeTabs.move).toHaveBeenCalledWith(9, { index: 0 });
     });
   });
 
-  describe('Moving Right', () => {
+  describe("Move To End", () => {
+    it("moves it to the end even if it is already there", async () => {
+      const TAB_STATE = [
+        buildTab(),
+        buildTab(),
+        buildTab({ highlighted: true, id: 9 }),
+      ].map(indexToTab);
 
-  });
+      (chromeTabs.query as jest.Mock).mockImplementation(
+        createMockQueryImplementationWith(TAB_STATE)
+      );
 
-  describe('Move To Start', () => {
-
-  })
-
-  describe('Move To End', () => {
-
-  });
-  it('is my first test', () => {
-    (chromeTabs.query as jest.Mock).mockImplementation((config) => {
-      return [];
+      await tabMovement("move_tab_to_end");
+      expect(chromeTabs.move).toHaveBeenCalledWith(9, { index: 2 });
     });
-    tabMovement('move_tab_left');
-    expect(false).not.toBeTruthy();
+
+    it("moves it to the end if it starts in the middle", async () => {
+      const TAB_STATE = [
+        buildTab(),
+        buildTab({ highlighted: true, id: 9 }),
+        buildTab(),
+      ].map(indexToTab);
+
+      (chromeTabs.query as jest.Mock).mockImplementation(
+        createMockQueryImplementationWith(TAB_STATE)
+      );
+
+      await tabMovement("move_tab_to_end");
+      expect(chromeTabs.move).toHaveBeenCalledWith(9, { index: 2 });
+    });
   });
 });
