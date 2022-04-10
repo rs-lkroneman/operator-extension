9c3786217d61b75a688238323e3da86bca16f558
Lennart Kroneman <a.l.kroneman@gmail.com>
2022-04-10 18:39:37 -0500

    stable version

--- src/background/background.ts
+++ src/background/background.ts
@@ -3,18 +3,10 @@ import commands from "../api/commands";
 
 import commandHandlers, { commandNames } from "../commands";
 
-type Memory = {
-  commands: string[];
-};
-
-const memory: Memory = {
-  commands: [],
-};
-
 const runCommand = (command: string) => {
   if (command === "REFRESH_COMMANDS") {
-    refreshCommands();
-    runtime.sendMessage(memory.commands);
+    const commands = refreshCommands();
+    runtime.sendMessage(commands);
     return;
   }
 
@@ -24,17 +16,12 @@ const runCommand = (command: string) => {
 };
 
 const refreshCommands = () => {
-  commands.getAll((result) => {
-    memory.commands = Array.from(
-      new Set([...commandNames, "REFRESH_COMMANDS"])
-    );
-  });
+  return Array.from(new Set([...commandNames, "REFRESH_COMMANDS"]));
 };
 
 commands.onCommand.addListener(runCommand);
-
 runtime.onConnect.addListener((port) => {
-  refreshCommands();
-  port.postMessage(memory.commands);
+  const commands = refreshCommands();
+  port.postMessage(commands);
   port.onMessage.addListener(runCommand);
 });
