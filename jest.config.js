c02749e56aae517a7a674fb8cba57a852117a45d
Lennart Kroneman <a.l.kroneman@gmail.com>
2022-06-13 19:49:18 -0500

    react testing library setup with typescript

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/test_utils/setup.ts"],
+  // globals: {
+  //   "ts-jest": {
+  //     // ts-jest configuration goes here
+  //     tsconfig: "<rootDir>/tsconfig.json",
+  //   },
+  // },
   collectCoverageFrom: [
     "src/**/*.{js,jsx,ts,tsx}",
     "!**/node_modules/**",
@@ -6,6 +15,7 @@ module.exports = {
     "!build/**",
   ],
   moduleNameMapper: {
+    "\\.(scss|css)$": "<rootDir>/src/__mocks__/styleMock.ts",
     "src/(.*)": "<rootDir>/src/$1",
     "test_utils/(.*)": "<rootDir>/test_utils/$1",
   },
